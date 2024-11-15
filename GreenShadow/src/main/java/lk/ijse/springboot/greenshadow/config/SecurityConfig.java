1c3e68b66a793d3338128f202188f8291b628904
Kavindu Senesh <ksenesh5@gmail.com>
2024-11-15 22:06:13 +0530

    bug fix in UserServiceImpl

---
+++ GreenShadow/src/main/java/lk/ijse/springboot/greenshadow/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+//package lk.ijse.springboot.greenshadow.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//
+//@Configuration
+//@EnableWebMvc
+//@RequiredArgsConstructor
+//@EnableMethodSecurity
+//public class SecurityConfig {
+//
+//}
---
+++ GreenShadow/src/main/java/lk/ijse/springboot/greenshadow/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package lk.ijse.springboot.greenshadow.exception;
+
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException() {
+  }
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+  public UserNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
