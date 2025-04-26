cbca435b7956d15f128c9a48a5763c3ca3595a32
Kavindu Senesh <ksenesh5@gmail.com>
2025-04-26 14:17:09 +0530

    Initial commit

---
+++ src/test/java/lk/ijse/crop_monitor/CropMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.crop_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
